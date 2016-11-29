683712ff055bb5de6cfd6b15c701bd4c8bd8854f
wuhy <sparklewhy@gmail.com>
2016-11-29 16:36:40 +0800

    update cache api

--- index.js
+++ index.js
@@ -59,12 +59,32 @@ module.exports = exports = {
     }
 };
 
-exports.scaffold = function (fis) {
+function initPkgManage(fis) {
     // 应用  fis-conf.js 自定义的配置
     var pkgManageConfig = exports.config;
     pkgManageConfig.initConfig(fis.config);
 
     pkgManageConfig.log = fis.log;
+}
+
+exports.cache = function (fis) {
+    initPkgManage(fis);
+
+    var cache = require('./lib/cache');
+    return {
+        showCacheDir: function (type) {
+            var dir = cache.getCacheDir(type);
+            fis.log.info('cache dir: %s', dir.green);
+        },
+        clearCache: function (type, components) {
+            cache.clearCache(type, components);
+            fis.log.info('clear cache done');
+        }
+    };
+};
+
+exports.scaffold = function (fis) {
+    initPkgManage(fis);
 
     return {
         download: wrapTimer(require('./lib/scaffold').download, 'Download scaffold'),
